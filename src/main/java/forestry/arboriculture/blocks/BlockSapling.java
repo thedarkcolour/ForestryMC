dfa7818d3675c7e8ab4608b01972a87d5cf43d4e
thedarkcolour <30441001+thedarkcolour@users.noreply.github.com>
2024-08-15 21:36:31 -0700

    Fix sapling drop Closes #36

--- src/main/java/forestry/arboriculture/blocks/BlockSapling.java
+++ src/main/java/forestry/arboriculture/blocks/BlockSapling.java
@@ -22,7 +22,6 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.BonemealableBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -34,11 +33,10 @@ import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-import forestry.api.arboriculture.genetics.TreeLifeStage;
 import forestry.api.arboriculture.genetics.ITree;
+import forestry.api.arboriculture.genetics.TreeLifeStage;
 import forestry.arboriculture.tiles.TileSapling;
 import forestry.core.tiles.TileUtil;
-import forestry.core.utils.ItemStackUtil;
 import forestry.core.utils.SpeciesUtil;
 
 public class BlockSapling extends BlockTreeContainer implements BonemealableBlock {
@@ -74,14 +72,13 @@ public class BlockSapling extends BlockTreeContainer implements BonemealableBloc
 	public void neighborChanged(BlockState state, Level worldIn, BlockPos pos, Block blockIn, BlockPos fromPos, boolean p_220069_6_) {
 		super.neighborChanged(state, worldIn, pos, blockIn, fromPos, p_220069_6_);
 		if (!worldIn.isClientSide && !canBlockStay(worldIn, pos)) {
-			dropAsSapling(worldIn, pos);
-			worldIn.setBlockAndUpdate(pos, Blocks.AIR.defaultBlockState());
+			worldIn.destroyBlock(pos, true);
 		}
 	}
 
 	@Override
 	public List<ItemStack> getDrops(BlockState blockState, LootContext.Builder builder) {
-		ItemStack drop = getDrop(builder.getLevel(), new BlockPos(builder.getParameter(LootContextParams.ORIGIN)));
+		ItemStack drop = getDrop(builder.getParameter(LootContextParams.BLOCK_ENTITY));
 		if (!drop.isEmpty()) {
 			return Collections.singletonList(drop);
 		}
@@ -97,33 +94,14 @@ public class BlockSapling extends BlockTreeContainer implements BonemealableBloc
 		return SpeciesUtil.TREE_TYPE.get().createStack(sapling.getTree(), TreeLifeStage.SAPLING);
 	}
 
-	@Override
-	public void playerDestroy(Level level, Player player, BlockPos pos, BlockState state, @Nullable BlockEntity blockEntity, ItemStack itemStack) {
-		if (!level.isClientSide && canHarvestBlock(state, level, pos, player) && !player.isCreative()) {
-			dropAsSapling(level, pos);
-		}
-
-		super.playerDestroy(level, player, pos, state, blockEntity, itemStack);
-	}
-
-	private static void dropAsSapling(Level world, BlockPos pos) {
-		if (world.isClientSide) {
-			return;
-		}
-		ItemStack drop = getDrop(world, pos);
-		if (!drop.isEmpty()) {
-			ItemStackUtil.dropItemStackAsEntity(drop, world, pos);
-		}
-	}
-
-	private static ItemStack getDrop(BlockGetter world, BlockPos pos) {
-		TileSapling sapling = TileUtil.getTile(world, pos, TileSapling.class);
-		if (sapling != null) {
+	private static ItemStack getDrop(BlockEntity blockEntity) {
+		if (blockEntity instanceof TileSapling sapling) {
 			ITree tree = sapling.getTree();
 			if (tree != null) {
 				return SpeciesUtil.TREE_TYPE.get().createStack(tree, TreeLifeStage.SAPLING);
 			}
 		}
+
 		return ItemStack.EMPTY;
 	}
 
